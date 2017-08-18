95c64451b7e869e3d29b60d75d83b5e409127302
Bill Zimmerlin <zjn700-01@yahoo.com>
2017-08-18 04:52:25 +0000

    help updates/improvements including return to top button

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-help',
@@ -6,20 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent implements OnInit {
-
+  public showReturnToTopButton=false;
+  
   constructor() { }
 
+  @HostListener('mouseover') onMouseOver() {
+    console.log('mouseover')
+    this.showReturnToTopButton = document.body.scrollTop > 1000
+    // this.glossaryOn = true;
+    // setTimeout(()=>this.glossaryOn = false, 2000)
+  }
+  
   ngOnInit() {
   }
   
   
   scrollToElement(elementId) {
+    console.log('document.body.scrollTop')
+    console.log(document.body.scrollTop)
     let element = document.getElementById(elementId);
     if (element) {
       element.scrollIntoView(true);
       document.body.scrollTop -= 60;
+      console.log(document.body.scrollTop)
+
     }
   }  
+  
+  returnToTop(){
+    window.scrollTo(0, 0);
+  }
   
   scrollTo(hash) {
     console.log('scrollTo ' + hash)
